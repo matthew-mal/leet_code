9c82d1edcd4322ece6a0d1445f5a9970a6560ac3
mat_mal <matvey_maljanov@rambler.ru>
2024-04-22 14:07:33 +0300

    20

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,17 @@
+def is_valid(s: str) -> bool:
+    stack = []
+    pairs = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+    }
+
+    for bracket in s:
+        if bracket in pairs:
+            stack.append(bracket)
+        elif len(stack) == 0 or bracket != pairs[stack.pop()]:
+            return False
+    return len(stack) == 0
+
+
+print(is_valid('()[]{]'))
