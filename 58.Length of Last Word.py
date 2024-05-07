51864a7816a2b6a6b0a3347421b5717ece157576
mat_mal <matvey_maljanov@rambler.ru>
2024-05-07 15:49:38 +0300

    58 вариант получше

--- 58.Length of Last Word.py
+++ 58.Length of Last Word.py
@@ -1,11 +1,12 @@
 def length_of_last_word(stroka: str) -> int:
-    if stroka[-1] == ' ':
-        new_str = stroka.strip(' ')
-        stroka2 = new_str.split(' ')
-        length = len(stroka2[-1])
+    # if stroka[-1] == ' ':
+    #     new_str = stroka.strip(' ')
+    #     stroka2 = new_str.split(' ')
+    #     length = len(stroka2[-1])
+    #
+    #     return length
+    # return len(stroka.split(' ')[-1])
+    return len(stroka.split()[-1])
 
-        return length
-    return len(stroka.split(' ')[-1])
 
-
-print(length_of_last_word('ghbdtn o fdf d fd fffffffff'))
+print(length_of_last_word('ghbdtn o fdf d fd ffffff                    '))
