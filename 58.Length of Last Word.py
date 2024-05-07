b95f32d7fef9d1c346d3b73991550cd5d70e7060
mat_mal <matvey_maljanov@rambler.ru>
2024-05-07 15:46:56 +0300

    58

---
+++ 58.Length of Last Word.py
@@ -0,0 +1,11 @@
+def length_of_last_word(stroka: str) -> int:
+    if stroka[-1] == ' ':
+        new_str = stroka.strip(' ')
+        stroka2 = new_str.split(' ')
+        length = len(stroka2[-1])
+
+        return length
+    return len(stroka.split(' ')[-1])
+
+
+print(length_of_last_word('ghbdtn o fdf d fd fffffffff'))
